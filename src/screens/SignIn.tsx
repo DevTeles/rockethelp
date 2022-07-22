efedc5b62f4194e1891b718815433a6403535fd8
Rafael <rteles78@gmail.com>
2022-07-21 23:41:19 -0300

    first commit

---
+++ src/screens/SignIn.tsx
@@ -0,0 +1,16 @@
+import { Heading, VStack } from "native-base";
+
+import Logo from '../assets/logo_primary.svg';
+
+export default function SignIn() {
+  return (
+    <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
+      
+      <Logo />
+      <Heading color="gray.100" fontSize="xl" mt={20} mb={6}>
+        Acesse sua conta
+      </Heading>
+
+    </VStack>
+  )
+}
