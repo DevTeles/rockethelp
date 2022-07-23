e2653f96e9ef51de8e637042a46cb057b8eabfb6
Rafael <rteles78@gmail.com>
2022-07-23 16:04:20 -0300

    add components button input signin

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,8 +1,13 @@
-import { Heading, VStack } from "native-base";
-
+import { Heading, VStack, Icon, useTheme } from "native-base";
+import { Envelope, Key } from 'phosphor-react-native';
 import Logo from '../assets/logo_primary.svg';
 
+import { Input } from "../components/Input";
+import { Button } from "../components/Button";
+
 export default function SignIn() {
+  const { colors } = useTheme();
+
   return (
     <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
       
@@ -11,6 +16,22 @@ export default function SignIn() {
         Acesse sua conta
       </Heading>
 
+      <Input
+        m={2}
+        w='full'
+        placeholder="E-mail" 
+        InputLeftElement={<Icon as={<Envelope color={colors.gray[300]} />} ml={4} />}
+      />
+      <Input 
+        m={2}
+        mb={8}
+        w='full'
+        placeholder="Senha"
+        InputLeftElement={<Icon as={<Key color={colors.gray[300]} />} ml={4} />}
+        secureTextEntry
+      />
+
+      <Button title="Entrar" w="full" />
     </VStack>
   )
 }
